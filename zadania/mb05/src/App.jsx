4d23a2737fa0cd385f30578da2fa3aa263563a30
dominikalazar <domi005x@gmail.com>
2026-09-21 23:26:37 +0200

    MB5: komponent CategoryBar

--- zadania/mb05/src/App.jsx
+++ zadania/mb05/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CategoryBar from "./components/CategoryBar";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -39,6 +40,10 @@ function App() {
           </div>
         </div>
       </header>
+
+      <main className="container">
+        <CategoryBar />
+      </main>
     </>
   );
 }
