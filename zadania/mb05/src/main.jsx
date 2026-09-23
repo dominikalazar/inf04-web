57b55e20556a1ab4a72d14d934fbad0cc3c1e602
dominikalazar <domi005x@gmail.com>
2026-09-23 10:31:58 +0200

    MB6: main.jsx importuje  modulowa wersje Bootstrapa

--- zadania/mb05/src/main.jsx
+++ zadania/mb05/src/main.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
